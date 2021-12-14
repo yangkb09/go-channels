df36de7edf0c8dcb83a058b018cb1b4a1545f139
yangkb09 <yangkb09@gmail.com>
2021-12-14 15:36:20 -0500

    Sec 7: Channel Implementation

--- main.go
+++ main.go
@@ -14,16 +14,21 @@ func main() {
 		"http://amazon.com",
 	}
 
+	c := make(chan string) // create new channel
+
 	for _, link := range links {
-		checkLink(link)
+		go checkLink(link, c)
 	}
+	fmt.Println(<-c)
 }
 
-func checkLink(link string) {
+func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
+		c <- "Might be down I think"
 		return
 	}
 	fmt.Println(link, "is up!")
+	c <- "Yep it is up"
 }
