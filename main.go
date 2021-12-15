6312c437d71dc9459f3af5cb81fbdeabf9aec70f
yangkb09 <yangkb09@gmail.com>
2021-12-15 14:39:33 -0500

    Sec 7: Repeating Routines

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ func main() {
 		go checkLink(link, c)
 	}
 
-	for i := 0; i < len(links); i++ {
-		fmt.Println(<-c)
+	for { // infinite loop; only loops anytime we receive something through a channel
+		go checkLink(<-c, c)
 	}
 }
 
@@ -29,9 +29,9 @@ func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
 		fmt.Println(link, "might be down!")
-		c <- "Might be down I think"
+		c <- link
 		return
 	}
 	fmt.Println(link, "is up!")
-	c <- "Yep it is up"
+	c <- link
 }
